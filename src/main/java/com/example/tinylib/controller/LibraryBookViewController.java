7b894da32752a71b224e66cdfa589f5c80bbb2d6
Antonios Alexopoulos <xviia42@gmail.com>
2023-12-01 09:33:25 +0200

    add doc and fix autowiring

--- src/main/java/com/example/tinylib/controller/LibraryBookViewController.java
+++ src/main/java/com/example/tinylib/controller/LibraryBookViewController.java
@@ -4,6 +4,7 @@ import com.example.tinylib.model.Book;
 import com.example.tinylib.model.responses.LibraryViewResponse;
 import com.example.tinylib.repository.LibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,12 +13,32 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Controller that handles requests in the /api/library/view_books
+ * endpoint.
+ */
 @RestController
 public class LibraryBookViewController {
 
-    @Autowired
+    /**
+     * The repository for accessing and retrieving the books from
+     * the library.
+     */
     private LibraryRepository libraryRepository;
 
+    /**
+     * Constructs a new LibraryBookViewController with the specified LibraryRepository.
+     * @param libraryRepository The LibraryRepository instance to be injected.
+     */
+    @Autowired
+    public LibraryBookViewController(LibraryRepository libraryRepository) {
+        this.libraryRepository = libraryRepository;
+    }
+
+    /**
+     * Handles requests in the /api/library/view_books endpoint.
+     * @return LibraryViewResponse An object with the available books in the library.
+     */
     @GetMapping(path="/api/library/view_books", produces = "application/json")
     public LibraryViewResponse ViewLibraryBooks() {
 
@@ -30,8 +51,11 @@ public class LibraryBookViewController {
 
         LocalDateTime timestampObj = LocalDateTime.now();
         DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
         return new LibraryViewResponse(
                 timestampObj.format(formatTime),
+                "/api/library/view_books",
+                HttpStatus.OK,
                 bookList.size(),
                 bookList);
     }
